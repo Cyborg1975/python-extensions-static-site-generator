aa8223148057075802ccad07477340174c1495dc
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2022-12-13 16:00:42 -0500

    Completed module

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -3,6 +3,6 @@ import importlib
 from pathlib import Path
 
 def load_module(directory, name):
-    sys.path.insert(directory, 0)
-    import_module(name)
+    sys.path.insert(0, directory)
+    importlib.import_module(name)
     sys.path.pop(0)
