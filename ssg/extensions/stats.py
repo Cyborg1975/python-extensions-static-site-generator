9e341f27255adc4687469c83e2add3041ef99ff5
Joel Perron <103043785+Cyborg1975@users.noreply.github.com>
2022-12-13 23:55:41 -0500

    Completed module

---
+++ ssg/extensions/stats.py
@@ -0,0 +1,21 @@
+from ssg import hooks
+import time
+
+
+start_time = None
+total_written = 0
+report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
+
+def start_build("start_build"):
+    global start_time = ctime()
+
+
+def written("written"):
+    global total_written = total_written + 1
+
+
+def stats("stats"):
+    final_time = ctime() - start_time
+    average = final_time / total_written if > 0
+
+print(report, format(total_written, final_time, average))
